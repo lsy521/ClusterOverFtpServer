6df434c7235c4938d0fe8cfcd17da2451d050197
Zz897426182 <dgxdks@163.com>
2017-05-18 09:35:44 +0800

    modify

--- src/main/java/com/hzgc/ftpserver/common/ClusterOverFtpServer.java
+++ src/main/java/com/hzgc/ftpserver/common/ClusterOverFtpServer.java
@@ -1,7 +1,11 @@
 package com.hzgc.ftpserver.common;
 
-import org.apache.ftpserver.DataConnectionConfiguration;
 import org.apache.ftpserver.DataConnectionConfigurationFactory;
+import org.apache.ftpserver.FtpServer;
+import org.apache.ftpserver.FtpServerFactory;
+import org.apache.ftpserver.listener.ListenerFactory;
+import org.apache.ftpserver.ssl.SslConfigurationFactory;
+import org.apache.ftpserver.usermanager.PropertiesUserManagerFactory;
 import org.apache.log4j.Logger;
 
 import java.io.File;
@@ -13,18 +17,19 @@ import java.util.Properties;
 public class ClusterOverFtpServer {
     private static Logger log = Logger.getLogger(ClusterOverFtpServer.class);
 
-    private static int port = 0;
+    private static int listenerPort = 0;
     private static int sslPort = 0;
     private static String passivePorts = null;
-    private static String sslPassivePorts = null;
+    private static String implicitSsl;
     private static String hdfsUri = null;
+    private static String password = null;
 
     private static DataConnectionConfigurationFactory dataConnConf;
 
     private static File loadResource(String resourceName) {
-        final URL resource = ClusterOverFtpServer.class.getResource(resourceName);
+        URL resource = ClusterOverFtpServer.class.getResource(resourceName);
         if (resource == null) {
-            log.error("Please check to see if there are any \"./conf/cluster-over-ftp.properties\"");
+            log.error(" Please check to see if there are any \"./conf/cluster-over-ftp.properties\" ");
             System.exit(1);
         }
         return new File(resource.getFile());
@@ -32,55 +37,100 @@ public class ClusterOverFtpServer {
 
     private static void loadConfig() throws IOException {
         Properties props = new Properties();
-        props.load(new FileInputStream(loadResource("/cluster-over-ftp.properties")));
+        dataConnConf = new DataConnectionConfigurationFactory();
+        props.load(new FileInputStream(loadResource("/conf/cluster-over-ftp.properties")));
 
         try {
-            port = Integer.parseInt(props.getProperty("port"));
-            checkPort(port, "ftpserver port");
-            log.info("The port:" + port + " for ftpserver is already set, FtpServer will be started");
+            listenerPort = Integer.parseInt(props.getProperty(" listener-port "));
+            checkPort(listenerPort, "listener port");
+            log.info(" The listener port:" + listenerPort + " for ftpserver is already set ");
         } catch (Exception e) {
-            log.info("The port for ftpserver is not set, Check that the FTP port is set");
+            log.error(" The port for listener is not set, Check that the \"listener-port\" is set ");
             System.exit(1);
         }
 
         try {
-            sslPort = Integer.parseInt(props.getProperty("ssl-port"));
-            log.warn("The port:" + sslPort +" for SSL is already set. SSL server will be started");
+            implicitSsl = props.getProperty("implicitSsl");
+            if ("true".equals(implicitSsl)) {
+                dataConnConf.setImplicitSsl(true);
+                log.info(" The implicitSsl = true is set, SSL server will be started ");
+            } else if ("false".equals(implicitSsl)){
+                log.info(" The implicitSsl use default config, the Ssl Server is closed ");
+            } else {
+                throw new Exception("The value settings for \"implicitSsl\" is illegal, check it");
+            }
         } catch (Exception e) {
-            log.warn("The port for SSL is not set, so SSL server is not applicable");
+            e.printStackTrace();
         }
 
-        if (port != 0) {
+        if (listenerPort != 0) {
             passivePorts = props.getProperty("data-ports");
             if (passivePorts == null) {
-                log.fatal("data-ports is not set");
-                System.exit(1);
+                log.info(" The data ports is not set, use any available port ");
+            } else {
+                dataConnConf.setPassivePorts(passivePorts);
+                log.info(" The data ports is set:" + passivePorts);
             }
         }
 
-        if (sslPort != 0) {
-            sslPassivePorts = props.getProperty("ssl-data-ports");
-            if (sslPassivePorts == null) {
-                log.fatal("ssl-data-ports is not set");
-                System.exit(1);
-            }
+//        if (sslPort != 0) {
+//            sslPassivePorts = props.getProperty("ssl-data-ports");
+//            if (sslPassivePorts == null) {
+//                log.fatal("ssl-data-ports is not set");
+//                System.exit(1);
+//            }
+//        }
+
+        password = props.getProperty("password");
+        if (password == null && password.length() <= 0) {
+            log.error(" Please set the ssl password, this is important ");
+            System.exit(1);
         }
 
-        hdfsUri = props.getProperty("hdfs-uri");
+        hdfsUri = props.getProperty("hdfs-url");
         if (hdfsUri == null) {
-            log.fatal("hdfs-uri is not set");
+            log.error(" The hdfs url is not set, this is important ");
             System.exit(1);
         }
 
         String superuser = props.getProperty("superuser");
         if (superuser == null) {
-            log.fatal("superuser is not set");
+            log.error(" The superuser is not set, please check it ");
             System.exit(1);
         }
     }
 
     public static void startFtpServer() throws Exception {
-        dataConnConf = new DataConnectionConfigurationFactory();
+        FtpServerFactory serverFactory = new FtpServerFactory();
+        ListenerFactory listenerFactory = new ListenerFactory();
+        //set the port of the listener
+        listenerFactory.setPort(listenerPort);
+
+        if ("true".equals(implicitSsl)) {
+            URL jksResource = ClusterOverFtpServer.class.getResource("/conf/ftpserver.jks");
+            URL userProperResource = ClusterOverFtpServer.class.getResource("/conf/users.properties");
+            if (jksResource == null) {
+                log.error(" Please check to see if there are any \"./conf/ftpserver.jks\" ");
+                System.exit(1);
+            }
+            SslConfigurationFactory sslFactory = new SslConfigurationFactory();
+            sslFactory.setKeystoreFile(new File(jksResource.getFile()));
+            sslFactory.setKeystorePassword(password);
+            // set the SSL configuration for the listener
+            listenerFactory.setSslConfiguration(sslFactory.createSslConfiguration());
+            listenerFactory.setImplicitSsl(true);
+            // replace the default listener
+            serverFactory.addListener("default", listenerFactory.createListener());
+            PropertiesUserManagerFactory userManagerFactory = new PropertiesUserManagerFactory();
+            if (userProperResource == null) {
+                log.error(" Please check to see if ther are any \"./conf/users.properties\" ");
+            }
+            userManagerFactory.setFile(new File(userProperResource.getFile()));
+            serverFactory.setUserManager(userManagerFactory.createUserManager());
+            //start the server
+            FtpServer server = serverFactory.createServer();
+            server.start();
+        }
 
     }
 
@@ -88,11 +138,13 @@ public class ClusterOverFtpServer {
         if (checkPort != 0 && checkPort > 0) {
             startFtpServer();
         } else {
-            log.error(" The port settings for " + server +" are illegal and must be greater than 1024 ");
+            log.error(" The port settings for " + server +" is illegal and must be greater than 1024 ");
+            System.exit(1);
         }
     }
 
     public static void main(String args[]) throws Exception {
         loadConfig();
+//        SslConfigurationFactory
     }
 }
---
+++ src/main/java/com/hzgc/ftpserver/common/Test.java
@@ -0,0 +1,12 @@
+package com.hzgc.ftpserver.common;
+
+import org.apache.ftpserver.util.EncryptUtils;
+
+/**
+ * Created by zhaozhe on 2017/5/16.
+ */
+public class Test {
+    public static void main(String args[]) {
+        System.out.println(EncryptUtils.encryptMD5("333"));
+    }
+}
