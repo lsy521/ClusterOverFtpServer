d71ea3e5a85d9ccdcf427177d572520659eef376
Zhaozhe <Zhaozhe>
2017-05-19 14:49:45 +0800

    appending

--- src/main/java/com/hzgc/ftpserver/common/ClusterOverFtpServer.java
+++ src/main/java/com/hzgc/ftpserver/common/ClusterOverFtpServer.java
@@ -18,18 +18,18 @@ public class ClusterOverFtpServer {
     private static Logger log = Logger.getLogger(ClusterOverFtpServer.class);
 
     private static int listenerPort = 0;
-    private static int sslPort = 0;
+//    private static int sslPort = 0;
     private static String passivePorts = null;
     private static String implicitSsl;
     private static String hdfsUri = null;
-    private static String password = null;
+    private static String sslPassword = null;
 
     private static DataConnectionConfigurationFactory dataConnConf;
 
     private static File loadResource(String resourceName) {
         URL resource = ClusterOverFtpServer.class.getResource(resourceName);
         if (resource == null) {
-            log.error(" Please check to see if there are any \"./conf/cluster-over-ftp.properties\" ");
+            log.error(" Please check to see if there are any " + resourceName);
             System.exit(1);
         }
         return new File(resource.getFile());
@@ -41,7 +41,7 @@ public class ClusterOverFtpServer {
         props.load(new FileInputStream(loadResource("/conf/cluster-over-ftp.properties")));
 
         try {
-            listenerPort = Integer.parseInt(props.getProperty(" listener-port "));
+            listenerPort = Integer.parseInt(props.getProperty("listener-port"));
             checkPort(listenerPort, "listener port");
             log.info(" The listener port:" + listenerPort + " for ftpserver is already set ");
         } catch (Exception e) {
@@ -81,10 +81,12 @@ public class ClusterOverFtpServer {
 //            }
 //        }
 
-        password = props.getProperty("password");
-        if (password == null && password.length() <= 0) {
-            log.error(" Please set the ssl password, this is important ");
-            System.exit(1);
+        if (implicitSsl != null && "true".equals(implicitSsl)) {
+            sslPassword = props.getProperty("password");
+            if (sslPassword == null) {
+                log.error(" Please set the ssl password, this is important ");
+                System.exit(1);
+            }
         }
 
         hdfsUri = props.getProperty("hdfs-url");
@@ -100,44 +102,38 @@ public class ClusterOverFtpServer {
         }
     }
 
-    public static void startFtpServer() throws Exception {
+    private static void startFtpServer() throws Exception {
         FtpServerFactory serverFactory = new FtpServerFactory();
         ListenerFactory listenerFactory = new ListenerFactory();
         //set the port of the listener
         listenerFactory.setPort(listenerPort);
+//        listenerFactory.setServerAddress("192.168.2.130");
 
         if ("true".equals(implicitSsl)) {
-            URL jksResource = ClusterOverFtpServer.class.getResource("/conf/ftpserver.jks");
-            URL userProperResource = ClusterOverFtpServer.class.getResource("/conf/users.properties");
-            if (jksResource == null) {
-                log.error(" Please check to see if there are any \"./conf/ftpserver.jks\" ");
-                System.exit(1);
-            }
+            //URL jksResource = ClusterOverFtpServer.class.getResource("/conf/ftpserver.jks");
+            //URL userProperResource = ClusterOverFtpServer.class.getResource("/conf/users.properties");
+
             SslConfigurationFactory sslFactory = new SslConfigurationFactory();
-            sslFactory.setKeystoreFile(new File(jksResource.getFile()));
-            sslFactory.setKeystorePassword(password);
+            sslFactory.setKeystoreFile(loadResource("/conf/ftpserver.jks"));
+            sslFactory.setKeystorePassword(sslPassword);
             // set the SSL configuration for the listener
             listenerFactory.setSslConfiguration(sslFactory.createSslConfiguration());
             listenerFactory.setImplicitSsl(true);
-            // replace the default listener
-            serverFactory.addListener("default", listenerFactory.createListener());
-            PropertiesUserManagerFactory userManagerFactory = new PropertiesUserManagerFactory();
-            if (userProperResource == null) {
-                log.error(" Please check to see if ther are any \"./conf/users.properties\" ");
-            }
-            userManagerFactory.setFile(new File(userProperResource.getFile()));
-            serverFactory.setUserManager(userManagerFactory.createUserManager());
-            //start the server
-            FtpServer server = serverFactory.createServer();
-            server.start();
         }
+        // replace the default listener
+        serverFactory.addListener("default", listenerFactory.createListener());
+        PropertiesUserManagerFactory userManagerFactory = new PropertiesUserManagerFactory();
+        userManagerFactory.setFile(loadResource("/conf/users.properties"));
+        serverFactory.setUserManager(userManagerFactory.createUserManager());
+//        serverFactory.setFileSystem();
+        //start the server
+        FtpServer server = serverFactory.createServer();
+        server.start();
 
     }
 
     public static void checkPort(int checkPort, String server) throws Exception {
-        if (checkPort != 0 && checkPort > 0) {
-            startFtpServer();
-        } else {
+        if (checkPort < 1024) {
             log.error(" The port settings for " + server +" is illegal and must be greater than 1024 ");
             System.exit(1);
         }
@@ -145,6 +141,6 @@ public class ClusterOverFtpServer {
 
     public static void main(String args[]) throws Exception {
         loadConfig();
-//        SslConfigurationFactory
+        startFtpServer();
     }
 }
